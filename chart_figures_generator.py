73e4bc2f809f5b73d7d3bc599d8d0901c06c6ee5
Maciej Fraszczak <218741@student.pwr.wroc.pl>
2020-05-23 21:00:19 +0200

    a

---
+++ chart_figures_generator.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# Software will be used to generate exemplary charts which will be used in thesis' figures.
+# Creating sets for exemplary points (objects of classification)
+
